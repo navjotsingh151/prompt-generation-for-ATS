83864ea1d7ac0cbd66f4a73a61ca60fb132f9d49
Navjot <navjotsingh151@gmail.com>
2024-03-26 21:54:25 -0400

    Added app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def prompt(input_text, jd):
     description:{jd}
 
     I want the response in one single string having the structure
-    {{"JD Match":"%","MissingKeywords:[]","Profile Summary":"","2 Job Duties from MissingKeywords":""}}
+    {{"JD Match":"%","MissingKeywords:[]","Profile Summary":"","2 Job Duties containing all MissingKeywords":""}}
     """
 
     return input_prompt
